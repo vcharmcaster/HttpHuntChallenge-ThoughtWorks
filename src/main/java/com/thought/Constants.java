68f2b28eb3401aeafbd75b0658eddd1ad72feee3
ArunPushparaj <44889836+ArunPushparaj@users.noreply.github.com>
2019-05-15 15:01:51 +0530

    Update Constants.java

--- src/main/java/com/thought/Constants.java
+++ src/main/java/com/thought/Constants.java
@@ -2,7 +2,7 @@ package com.thought;
 
 public class Constants {
 
-	public static final String userID = "heCQKLKqr";
+	public static final String userID = "<Your User Id>";
 	public static final String URI = "https://http-hunt.thoughtworks-labs.net/challenge";
 	public static final String CHALLENGE_OUTPUT_PATH = "https://http-hunt.thoughtworks-labs.net/challenge/output";
 	public static final String CHALLENGE_INPUT_PATH = "https://http-hunt.thoughtworks-labs.net/challenge/input";
